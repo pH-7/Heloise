406aec1b55985b7be220d618a5a57e1e6fb48b5e
Pierre-Henry Soria <hi@ph7.me>
2018-07-28 22:55:35 +0200

    Add Post Feed tests

---
+++ tests/Feature/PostFeedTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Teapot\StatusCode;
+use Tests\TestCase;
+
+class PostFeedTest extends TestCase
+{
+    public function testCanReachFeedPage(): void
+    {
+        $response = $this->get('/feed');
+        $response->assertStatus(StatusCode::OK);
+    }
+
+    public function testCanSeeFeed(): void
+    {
+        $post = factory(PostModel::class)->create();
+
+        $response = $this->get('/feed');
+        $response->assertStatus(StatusCode::OK);
+        $response->assertSee($post->title);
+    }
+}
