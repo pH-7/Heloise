83b45ede22c093ed0747fe314f5c2f037e4eec66
Pierre-Henry Soria <hi@ph7.me>
2018-07-28 17:11:26 +0200

    Remove useless User::redirectTo() method

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,9 +28,4 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-
-    protected function redirectTo()
-    {
-        return '/';
-    }
 }
