7833cec499e3744f068ef48ad46beee6d75daf95
Pierre-Henry Soria <hi@ph7.me>
2018-07-28 19:04:59 +0200

    [Route:Web] Change URL path + add comment

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 Route::get('/', 'PostController@index')->name('homepage');
+
+// Redirect /home to root path used in Auth Middleware
 Route::get('/home', function () {
     return redirect('/');
 });
 
 Auth::routes();
 Route::resource('/post','PostController', ['except' => ['index']]);
-Route::post('/{id}/comment','CommentController@store')->where('id', '[0-9]+');
+Route::post('/post/{id}/comment','CommentController@store')->where('id', '[0-9]+');
