69adad3205736bede7e535410d11b0624b12a04b
Pierre-Henry Soria <hi@ph7.me>
2018-07-28 17:20:06 +0200

    [Fix] Was typo in method name (creat() -> create())

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(App\Models\Post::class, function (Faker $faker) {
     return [
         'user_id' => function(){
-            return factory(App\Models\User::class)->creat()->id;
+            return factory(App\Models\User::class)->create()->id;
         },
         'title' => $faker->sentence,
         'body' => $faker->paragraph
