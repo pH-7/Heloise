0a26f8eb48605aa5407df215e37d2c2c63ed214c
Pierre-Henry Soria <hi@ph7.me>
2018-07-29 12:06:32 +0200

    Redirect on edit page after updating post
    Rather than redirecting on homepage

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -94,7 +94,7 @@ class PostController extends Controller
         $post->save();
 
         return redirect()
-            ->route('homepage')
+            ->route('post.edit', $id)
             ->with('success', 'Post updated!');
     }
 
