fc0f32ff3e4f5cac8911ffd544ba0390b614707c
Pierre-Henry Soria <hi@ph7.me>
2018-07-29 21:58:46 +0200

    Fix issue with Edit/Delete post buttons

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -4,26 +4,26 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-12">
+        <div class="col col-md-12">
             <article>
                 <h3 class="article-title text-center">{{ $post->title }}</h3>
                 <div class="collapsible-body text-center">{{ $post->body }}</div>
             </article>
-        </div>
-        @auth
-            <hr />
-            <div class="text-center">
-                {!! Form::open(['route' => ['post.edit',  $post->id], 'method' => 'get'])!!}
-                {{ Form::submit('Edit Post', ['class' => 'paper-btn']) }}
-                {!! Form::close() !!}
+            @auth
+                <hr />
+                <div class="text-center">
+                    {!! Form::open(['route' => ['post.edit',  $post->id], 'method' => 'get', 'class' => 'inline'])!!}
+                    {{ Form::submit('Edit Post', ['class' => 'paper-btn']) }}
+                    {!! Form::close() !!}
 
-                {!! Form::open(['route' => ['post.destroy',  $post->id]])!!}
-                {{ Form::token() }}
-                {{ Form::hidden('_method', 'DELETE') }}
-                {{ Form::submit('Delete Post', ['class' => 'paper-btn btn-danger']) }}
-                {!! Form::close() !!}
-            </div>
-        @endauth
+                    {!! Form::open(['route' => ['post.destroy',  $post->id], 'class' => 'inline'])!!}
+                    {{ Form::token() }}
+                    {{ Form::hidden('_method', 'DELETE') }}
+                    {{ Form::submit('Delete Post', ['class' => 'paper-btn btn-danger']) }}
+                    {!! Form::close() !!}
+                </div>
+            @endauth
+        </div>
     </div>
     @include('comment._thread')
 @endsection
