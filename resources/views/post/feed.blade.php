1f2dfc920de92ebf03cbce5a03e7aaf952939cd3
Pierre-Henry Soria <hi@ph7.me>
2018-07-29 02:35:32 +0200

    [Feed] Foreach posts to show the 10 latest posts

--- resources/views/post/feed.blade.php
+++ resources/views/post/feed.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.rss')
 
 @section('content')
-    <item>
-        <title>{{ $post->title }}</title>
-        <link>{{ action('PostController@show', [$post->id]) }}</link>
-        <pubDate>{{ $post->created_at->format('r') }}</pubDate>
-        <description><![CDATA[{{ $post->body }}]]></description>
-    </item>
+    @foreach($posts as $post)
+        <item>
+            <title>{{ $post->title }}</title>
+            <link>{{ action('PostController@show', [$post->id]) }}</link>
+            <pubDate>{{ $post->created_at->format('r') }}</pubDate>
+            <description><![CDATA[{{ $post->body }}]]></description>
+        </item>
+    @endforeach
 @endsection
