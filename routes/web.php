51ad0e73f6fd3a5f571883a29617344ec08e3a70
Pierre-Henry Soria <hi@ph7.me>
2018-07-29 12:10:11 +0200

    [Route] Give a name to PostFeedController

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,4 @@ Auth::routes();
 Route::resource('/post','PostController', ['except' => ['index']]);
 Route::get('/post/{postId}/add-comment','CommentController@create')->where('postId', '[0-9]+')->name('comment.create');
 Route::post('/post/{postId}/submit-comment','CommentController@store')->where('postId', '[0-9]+')->name('comment.store');
-Route::get('/feed', 'PostFeedController@index');
+Route::get('/feed', 'PostFeedController@index')->name('post.feed.index');
