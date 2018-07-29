02b700e8ae5bcd82ddb18bd4f784af7bc2cbbe6f
Pierre-Henry Soria <hi@ph7.me>
2018-07-29 13:56:30 +0200

    Fade out alert msg after 10sec

--- resources/views/partials/_alert.blade.php
+++ resources/views/partials/_alert.blade.php
@@ -1,9 +1,17 @@
 @foreach (['alert-success' => 'success', 'alert-danger' => 'error'] as $cssClass => $sessName)
     @if (session($sessName))
         <div class="col-md-6 row">
-            <p class="alert {{ $cssClass }}">
+            <p class="alert {{ $cssClass }}" role="alert">
                 {{ session($sessName) }}
             </p>
         </div>
     @endif
 @endforeach
+
+<script>
+    $(function() {
+        if ($('div[role=alert]').length) {
+            $('div[role=alert]').fadeOut(10000);
+        }
+    });
+</script>
