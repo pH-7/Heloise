7d569da8a6c0999da4a7b1a24d37d2dd01a72d5d
Pierre-Henry Soria <hi@ph7.me>
2018-07-29 16:44:26 +0200

    Add inline docblock for Factory class

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,7 +1,9 @@
 <?php
 
 use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
 
+/** @var Factory $factory */
 $factory->define(App\Models\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
