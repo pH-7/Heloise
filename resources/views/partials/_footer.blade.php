069be315cd737bee915cee5ae8b54cf3534c0924
Pierre-Henry Soria <hi@ph7.me>
2018-07-29 02:36:47 +0200

    Improve CSS/Style! :art:

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -2,7 +2,7 @@
     <p class="text-center">
         Made with ❤️ by <a href="https://github.com/ph-7">Pierre-Henry</a> •
         <a href="{{ action('PostFeedController@index') }}">
-            <img src="{{ asset('/img/feed.svg') }}" alt="RSS Feed" title="Feed" />
+            <img src="{{ asset('/img/feed.svg') }}" class="inline" alt="RSS Feed" title="Feed" />
         </a>
     </p>
 </footer>
