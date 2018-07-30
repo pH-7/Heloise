6aa6310b2f4fcfee922c6b5ac155514632e99c04
Pierre-Henry Soria <hi@ph7.me>
2018-07-30 12:03:16 +0200

    Store my Github URL into env app config file

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
     <p class="text-center">
-        Made with ❤️ by <a href="https://github.com/ph-7">Pierre-Henry</a> •
+        Made with ❤️ by <a href="{{ config('app.author_url') }}">Pierre-Henry</a> •
         <a href="{{ route('post.feed.index') }}">
             <img src="{{ asset('/img/feed.svg') }}" class="inline" alt="RSS Feed" title="Feed" />
         </a>
