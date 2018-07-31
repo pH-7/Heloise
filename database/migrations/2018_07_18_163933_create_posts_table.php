4a5f7ac05444f02333a77dff5e6339e29d6cad72
Pierre-Henry Soria <hi@ph7.me>
2018-07-31 14:54:26 +0200

    [Database:Migration] Cleanup Indentation

--- database/migrations/2018_07_18_163933_create_posts_table.php
+++ database/migrations/2018_07_18_163933_create_posts_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreatePostsTable extends Migration
 {
@@ -16,7 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
-            $table->string('title',50);
+            $table->string('title', 50);
             $table->text('body');
             $table->integer('user_id');
         });
